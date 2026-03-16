c0a320801991662f8e218eca15b235821c5e5a14
Manuel-Garcia-Diaz <manuel.garcia-d.alu@santamaria-artesano.org>
2026-03-16 11:59:21 +0100

    Update Borrame.java
    Hecho en el remoto

--- src/borrame/Borrame.java
+++ src/borrame/Borrame.java
@@ -1,4 +1,4 @@
-/*
+26 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -14,7 +14,7 @@ public class Borrame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here es 16 de marzo
     }
     
 }
